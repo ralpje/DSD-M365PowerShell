7c62be46def095953f0171657a6c8f4f03e54233
Ralph Eckhard <r.eckhard@peopleware.nl>
2020-09-17 16:23:19 +0200

    Code snippts from training

--- SfB Online PowerShell cmdlets.ps1
+++ SfB Online PowerShell cmdlets.ps1
@@ -1,16 +1,22 @@
+# verbinding maken met SfB / Teams Online
 import-module SkypeOnlineConnector
 $sfbsession = New-CsOnlineSession
 import-pssession $sfbsession
 
 
+# Online Voice user ophalen
+get-csonlineuser username
 
-
-
-$user = get-csonlineuser username
-
-
-
-
-
+# Voice Routes ophalen
 get-csonlinevoiceroute
-Get-CsOnlinePSTNGateway
+
+# Voice Route aanmaken
+New-CSONlineVoiceRoute -identiy Belgium -NumberPattern "^\+32(\d[9])$" -OnlinePSTNGatewayList "klantdomein.provider.tld"
+
+# Aanmaken Auto Attendant via PowerShell
+
+$GreetingPrompt = New-CsAutoAttendantPrompt -TextToSpeechPrompt “Welkom bij ons bedrijf"
+$automaticMenuOption = New-CsAutoAttendantMenuOption -Action Disconnect -DtmfResponse Automatic
+$Menu = New-CsAutoAttendantMenu -Name “Keuzemenu” -MenuOptions @($automaticMenuOption)
+$CallFlow = New-CsAutoAttendantCallFlow -Name “Call Flow” -Greetings @($GreetingPrompt) -Menu $Menu
+New-CsAutoAttendant -Name "Auto Attendant" -Language "NL-NL" -TimeZoneId "UTC" -DefaultCallFlow $CallFlow
