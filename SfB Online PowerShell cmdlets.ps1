d45e7a5630b818fca3e3c3b0eca43e1832c37282
Ralph Eckhard <ralph@ralpheckhard.com>
2020-10-14 15:40:08 +0200

    updated sfb cmdlets

--- SfB Online PowerShell cmdlets.ps1
+++ SfB Online PowerShell cmdlets.ps1
@@ -11,12 +11,16 @@ get-csonlineuser username
 get-csonlinevoiceroute
 
 # Voice Route aanmaken
-New-CSONlineVoiceRoute -identiy Belgium -NumberPattern "^\+32(\d[9])$" -OnlinePSTNGatewayList "klantdomein.provider.tld"
+New-CSONlineVoiceRoute -identiy Belgium -NumberPattern "^\+32(\d[9])$" -OnlinePSTNGatewayList "dsd-europe-test-client-92c04.msteamsoperator.eu"
 
 # Aanmaken Auto Attendant via PowerShell
 
-$GreetingPrompt = New-CsAutoAttendantPrompt -TextToSpeechPrompt “Welkom bij ons bedrijf"
+$GreetingPrompt = New-CsAutoAttendantPrompt -TextToSpeechPrompt "Welkom bij ons bedrijf"
 $automaticMenuOption = New-CsAutoAttendantMenuOption -Action Disconnect -DtmfResponse Automatic
-$Menu = New-CsAutoAttendantMenu -Name “Keuzemenu” -MenuOptions @($automaticMenuOption)
-$CallFlow = New-CsAutoAttendantCallFlow -Name “Call Flow” -Greetings @($GreetingPrompt) -Menu $Menu
+$Menu = New-CsAutoAttendantMenu -Name "Keuzemenu" -MenuOptions @($automaticMenuOption)
+$CallFlow = New-CsAutoAttendantCallFlow -Name "Call Flow" -Greetings @($GreetingPrompt) -Menu $Menu
 New-CsAutoAttendant -Name "Auto Attendant" -Language "NL-NL" -TimeZoneId "UTC" -DefaultCallFlow $CallFlow
+
+# Koppelen telefoonnummer aan gebruiker
+Get-CsOnlineLisLocation
+Set-CsOnlineVoiceUser -Identity voip-config@dsd-europe-test-client-92c04.msteamsoperator.eu -TelephoneNumber "+317320011200"
