38f9d642e4090e7b93af473e42adaceadd2ca262
Ralph Eckhard <ralph@ralpheckhard.com>
2020-10-14 12:47:00 +0200

    Added exchange info

---
+++ Exchange Online Statistics.ps1
@@ -0,0 +1,5 @@
+﻿$mailboxes = get-exomailbox | where {$_.displayname -ne 'Discovery Search Mailbox'}
+foreach ($mailbox in $mailboxes) {
+    Get-MailboxStatistics -Identity $mailbox.identity | select DisplayName, TotalItemSize
+
+}
